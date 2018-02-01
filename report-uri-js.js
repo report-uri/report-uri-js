5d15be34e6be5facecca2af4d1f562841cdd7fab
Scott Helme <scotthelme@hotmail.com>
2018-02-01 10:40:58 +0100

    Use spec compliant format.
    This will make Report URI JS compatible with any other reporting
    service that ingests spec compliant CSP reports.

--- report-uri-js.js
+++ report-uri-js.js
@@ -1,9 +1,10 @@
 document.addEventListener('securitypolicyviolation', function (event) {
     var config = JSON.parse(document.getElementById('csp-report-uri').textContent);
+    var reportKeys = {'blockedURI':'blocked-uri', 'columnNumber':'column-number', 'documentURI':'document-uri', 'effectiveDirective':'effective-directive', 'lineNumber':'line-number', 'originalPolicy':'original-policy', 'sourceFile':'source-file', 'statusCode':'status-code', 'violatedDirective':'violated-directive'};
     var json = {'csp-report': {}};
     for (var i = 0, len = config.keys.length; i < len; i++) {
         if (event[config.keys[i]] !== 0 && event[config.keys[i]] !== '') {
-            json['csp-report'][config.keys[i]] = event[config.keys[i]];
+            json['csp-report'][(reportKeys[config.keys[i]] ? reportKeys[config.keys[i]] : config.keys[i])] = event[config.keys[i]];
         }
     }
     var xhr = new XMLHttpRequest();
